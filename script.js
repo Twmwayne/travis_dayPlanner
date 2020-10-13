3d649faa512f810213ef5450f165d7ede4687947
Travis Mendenhall <twmwayne@gmail.com>
2020-10-13 15:04:20 -0600

    started clock and display time for js

--- script.js
+++ script.js
@@ -22,4 +22,50 @@ $(document).ready(function() {
         planTextArr[4] = "Dont forget Lunch";
     }
 
+
+    let $plannerDiv = $('#plannerContainer');
+    $plannerDiv.empty();
+
+    if (test) { console.log("current time",nowHour12); }
+
+    for (let hour = 9; hour <= 17; hour++) {
+        let index = hour - 9;
+
+        let $rowDiv = $('<div>');
+        $rowDiv.addClass('row');
+        $rowDiv.addClass('plannerRow');
+        $rowDiv.attr('hour-index',hour);
+
+        let $col2SaveDiv = $('<div>');
+        $col2SaveDiv.addClass('col-md-2');
+
+        let $col2TimeDiv = $('<div>');
+        $col2TimeDiv.addClass('col-md-2');
+
+        const $timeBoxSpn = $('<span>');
+        $timeBoxSpn.attr('class','timeBox');
+
+        let displayHour = 0;
+        let ampm = "";
+        if (hour > 12) {
+            displayHour = hour - 12;
+            ampm = "pm";
+        }
+        else {
+            displayHour = hour;
+            ampm = "am";
+        }
+
+        $timeBoxSpn.text(`${displayHour}) ${ampm}`);
+
+
+
+
+
+
+
+
+
+     }
+
 })
